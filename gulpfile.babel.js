e170b8461876f773b7f146e514295c1e6b001f29
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2017-04-07 17:17:20 +0300

    Added gulp tasks

---
+++ gulpfile.babel.js
@@ -0,0 +1,50 @@
+import gulp from 'gulp';
+import eslint from 'gulp-eslint';
+import babel from 'gulp-babel';
+import newer from 'gulp-newer';
+import jade from 'gulp-jade';
+import nodemon from 'gulp-nodemon';
+
+const paths = {
+  js: ['./**/*.js', '!dist/**', '!node_modules/**', '!public/lib/**'],
+  static: ['./package.json', 'public/lib/**'],
+  views: ['./public/views/*']
+};
+
+gulp.task('lint', () => {
+  return gulp.src(paths.js)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('copy', () => {
+  gulp.src([...paths.static], { base: '.' })
+    .pipe(newer('dist'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('jade', () => {
+  gulp.src(paths.views, { base: '.' })
+    .pipe(newer('dist'))
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['copy'], () => {
+  gulp.src([...paths.js], { base: '.' })
+    .pipe(newer('dist'))
+    .pipe(babel())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['build'], () => {
+  nodemon({
+    script: 'dist/app.js',
+    ext: 'js',
+    ignore: ['node_modules/**/*.js', 'dist/**/*.js'],
+    tasks: ['copy', 'build']
+  });
+});
