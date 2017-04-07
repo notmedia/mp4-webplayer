fc853fca15eb0a8c8de73496f2569329aecd0ba5
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2017-04-08 01:25:11 +0300

    Used path.join instead +

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import http from 'http';
 import socketio from 'socket.io';
+import path from 'path';
 
 const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-app.use(express.static('./dist/public'));
+app.use('/', express.static(path.join(__dirname, '/public')));
 
 io.on('connection', socket => {
 });
