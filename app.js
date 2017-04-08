cb8c729a345585da58a25cf99c6ac1d117fddf14
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2017-04-08 12:27:12 +0300

    Added socket connection

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ import path from 'path';
 
 const app = express();
 const server = http.Server(app);
-const io = socketio(server);
+const io = socketio(server, { 'transports': ['websocket'] });
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 io.on('connection', socket => {
+  console.log('connection succeeded');
 });
 
-server.listen(8083);
+server.listen(8080);
--- public/app.js
+++ public/app.js
@@ -1,4 +1,21 @@
-const app = angular.module('client', []);
+const app = angular.module('client', ['btford.socket-io']);
 
-app.run(() => {
+app.factory('socket', (socketFactory) => {
+  return socketFactory({
+    ioSocket: io.connect('/', { transports: ['websocket'] })
+  });
+});
+
+app.controller('main', () => {
+
+});
+
+app.controller('video', () => {
+
+});
+
+app.run((socket) => {
+  socket.on('connect', () => {
+    console.log('connected');
+  });
 });
