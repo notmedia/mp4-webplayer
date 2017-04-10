38fe7e1c7453c2dd40d8842153b4ae7b19d737b2
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2017-04-10 21:10:11 +0300

    Setted chunk size for read stream

--- src/helpers/videos.js
+++ src/helpers/videos.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import fs from 'fs';
 
+const CHUNK_SIZE = 128*1024;
+
 function get(path, callback) {
   fs.readdir(path, (error, files) => {
     if (error) {
@@ -16,7 +18,7 @@ function get(path, callback) {
 }
 
 function watch(path, name) {
-  return fs.createReadStream(`${path}/${name}.mp4`);
+  return fs.createReadStream(`${path}/${name}.mp4`, { highWaterMark: CHUNK_SIZE });
 }
 
 export default { get, watch };
